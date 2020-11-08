3c9026f62a909089b7a093c90e0dd3084bab38da
AlexLuthor09 <capalex@hotmail.be>
2020-11-08 11:15:31 +0100

    connectionstrin + sql server express

--- Inscription Ocarina/SQL_Manager.cs
+++ Inscription Ocarina/SQL_Manager.cs
@@ -7,6 +7,8 @@ using System.Data;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration.Install;
+using System.Configuration;
 using System.Windows.Forms;
 using System.IO;
 
@@ -18,197 +20,209 @@ namespace Inscription_Ocarina
         SqlDouble prixmc = 1.5;
         SqlDouble nprixmc = 6.5;
 
-        private string connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Visual Studio\Inscription Ocarina\Inscription Ocarina\IncriptionOcarina.mdf; Integrated Security = True; Connect Timeout = 30";
         SqlConnection cnn;
-       
+
+        string connetionString = ConfigurationManager.ConnectionStrings["IncriptionOcarinaConnectionString"].ConnectionString;
+        
         public SQL_Manager() //test de connection
         {
-
-            cnn = new SqlConnection(connetionString);
-
-            try
+            using (cnn = new SqlConnection(connetionString))
             {
-                cnn.Open();
-                //MessageBox.Show("Connection Open ! ");
-                cnn.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("On a pas pu ce connecter a la database ! \n " + ex);
+
+                try
+                {
+                    cnn.Open();
+                    MessageBox.Show("Connection Open ! ");
+                    cnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("On a pas pu ce connecter a la database ! \n " + ex);
+                }
             }
         }
         public void addChildren(string nom, string prenom, int age, DateTime date, string email, int N_national, string adresse, bool mc, bool Fiche_Sante, string Allergies, string Remarque)
         {
 
-            cnn = new SqlConnection(connetionString);
+            using (cnn = new SqlConnection(connetionString))
+            {
 
-            string Query = @"INSERT INTO Enfant  (Nom,Prenom,Email,N_Nationam,Date_Naissance,Age,MC,Fiche_Sante,Remarque,Allergie,Adresse,Prix)" +
+                string Query = @"INSERT INTO Enfant  (Nom,Prenom,Email,N_Nationam,Date_Naissance,Age,MC,Fiche_Sante,Remarque,Allergie,Adresse,Prix)" +
                 "SELECT @NOM,@PRENOM,@EMAIL,@N_NATIONAM,@DATE_NAISSANCE,@AGE,@MC,@FICHE_SANTE,@REMARQUE,@ALLERGIE,@ADRESSE,@PRIX";
-            
 
-            SqlDateTime da = date;
-            
-            try
-            {
-             
-                cnn.Open();
-                SqlCommand addChild = new SqlCommand(Query, cnn);                
-                addChild.Parameters.AddWithValue("@NOM", nom);               
-                addChild.Parameters.AddWithValue("@PRENOM", prenom);
-                addChild.Parameters.AddWithValue("@EMAIL", email);
-                addChild.Parameters.AddWithValue("@N_NATIONAM", N_national);
-                addChild.Parameters.AddWithValue("@DATE_NAISSANCE", da);
-                addChild.Parameters.AddWithValue("@AGE", age);
-                addChild.Parameters.AddWithValue("@MC", mc);               
-                addChild.Parameters.AddWithValue("@FICHE_SANTE", Fiche_Sante);
-                addChild.Parameters.AddWithValue("@REMARQUE", Remarque);
-                addChild.Parameters.AddWithValue("@ALLERGIE", Allergies);
-                addChild.Parameters.AddWithValue("@ADRESSE", adresse);
-                if (mc)
-                    addChild.Parameters.AddWithValue("@PRIX", prixmc);
 
-                else
-                    addChild.Parameters.AddWithValue("@PRIX", nprixmc);
-                if (addChild.ExecuteNonQuery() == 0)
-                    throw new ApplicationException("Aucune ligne insérée, vérifiez les paramètres!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
+                SqlDateTime da = date;
 
-                cnn.Close();
+                try
+                {
+
+                    cnn.Open();
+                    SqlCommand addChild = new SqlCommand(Query, cnn);
+                    addChild.Parameters.AddWithValue("@NOM", nom);
+                    addChild.Parameters.AddWithValue("@PRENOM", prenom);
+                    addChild.Parameters.AddWithValue("@EMAIL", email);
+                    addChild.Parameters.AddWithValue("@N_NATIONAM", N_national);
+                    addChild.Parameters.AddWithValue("@DATE_NAISSANCE", da);
+                    addChild.Parameters.AddWithValue("@AGE", age);
+                    addChild.Parameters.AddWithValue("@MC", mc);
+                    addChild.Parameters.AddWithValue("@FICHE_SANTE", Fiche_Sante);
+                    addChild.Parameters.AddWithValue("@REMARQUE", Remarque);
+                    addChild.Parameters.AddWithValue("@ALLERGIE", Allergies);
+                    addChild.Parameters.AddWithValue("@ADRESSE", adresse);
+                    if (mc)
+                        addChild.Parameters.AddWithValue("@PRIX", prixmc);
+
+                    else
+                        addChild.Parameters.AddWithValue("@PRIX", nprixmc);
+                    if (addChild.ExecuteNonQuery() == 0)
+                        throw new ApplicationException("Aucune ligne insérée, vérifiez les paramètres!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+
+                    cnn.Close();
+                }
             }
         }
         public void updateChildren(int ID, string nom, string prenom, int age, DateTime date, string email, int N_national, string adresse, bool mc, bool Fiche_Sante, string Allergies, string Remarque)
         {
 
-            cnn = new SqlConnection(connetionString);        
-            SqlDouble p;
-            if (mc)
-                 p = prixmc;
-
-            else
-                p = nprixmc;
-
-            SqlDateTime da = date;
-            string Query = "UPDATE Enfant SET " +
-                "Nom ='" + nom +
-                "',Prenom ='" + prenom +
-                "',Email ='" + age +
-                "',N_Nationam ='" + N_national +
-                "',Date_Naissance ='" + da +
-                "',Age ='" + age +
-                "',MC ='" + mc +
-                "',Fiche_Sante ='" + Fiche_Sante +
-                "',Prix ='"+ p.ToSqlDecimal() +
-                "',Remarque ='" + Remarque +
-                "',Allergie ='" + Allergies +
-                "',Adresse = '" + adresse +
-                "' WHERE Id =" + ID + ";";
-
-            try
+            using (cnn = new SqlConnection(connetionString))
             {
-                cnn.Open();
+                SqlDouble p;
+                if (mc)
+                    p = prixmc;
 
-                SqlCommand modifchild = new SqlCommand(Query, cnn);
-                if (modifchild.ExecuteNonQuery() == 0)
-                    throw new ApplicationException("y a r de modif !");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
-            }
-            finally
-            {
+                else
+                    p = nprixmc;
 
-                cnn.Close();
-            }
+                SqlDateTime da = date;
+                string Query = "UPDATE Enfant SET " +
+                    "Nom ='" + nom +
+                    "',Prenom ='" + prenom +
+                    "',Email ='" + age +
+                    "',N_Nationam ='" + N_national +
+                    "',Date_Naissance ='" + da +
+                    "',Age ='" + age +
+                    "',MC ='" + mc +
+                    "',Fiche_Sante ='" + Fiche_Sante +
+                    "',Prix ='" + p.ToSqlDecimal() +
+                    "',Remarque ='" + Remarque +
+                    "',Allergie ='" + Allergies +
+                    "',Adresse = '" + adresse +
+                    "' WHERE Id =" + ID + ";";
 
+                try
+                {
+                    cnn.Open();
+
+                    SqlCommand modifchild = new SqlCommand(Query, cnn);
+                    if (modifchild.ExecuteNonQuery() == 0)
+                        throw new ApplicationException("y a r de modif !");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
+                }
+                finally
+                {
+
+                    cnn.Close();
+                }
+
+            }
         }
         public void refresh(DataGridView datagridview, ComboBox combobox)
         {
 
-            cnn = new SqlConnection(connetionString);
-            string Query = "SELECT * from Enfant";
-            
-            try
+            using (cnn = new SqlConnection(connetionString))
             {
-                cnn.Open();
-                SqlDataAdapter da = new SqlDataAdapter(Query, cnn);
-                IncriptionOcarinaDataSet ds = new IncriptionOcarinaDataSet();
-                da.Fill(ds, "Enfant");
+                string Query = "SELECT * from Enfant";
 
-                combobox.DataBindings.Clear();
-                combobox.DataSource = null;
-                combobox.DataSource = ds.Enfant; //attention c'est pour savoir ou aller chercher
-                combobox.DisplayMember = "Nom";
-                combobox.ValueMember = "Id";
+                try
+                {
+                    cnn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(Query, cnn);
+                    IncriptionOcarinaDataSet ds = new IncriptionOcarinaDataSet();
+                    da.Fill(ds, "Enfant");
 
-                datagridview.DataSource = ds.Enfant;
+                    combobox.DataBindings.Clear();
+                    combobox.DataSource = null;
+                    combobox.DataSource = ds.Enfant; //attention c'est pour savoir ou aller chercher
+                    combobox.DisplayMember = "Nom";
+                    combobox.ValueMember = "Id";
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
-            }
-            finally
-            {
+                    datagridview.DataSource = ds.Enfant;
 
-                cnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
+                }
+                finally
+                {
+
+                    cnn.Close();
+                }
             }
         }
         public void ClearDataBase()
         {
-            cnn = new SqlConnection(connetionString);
-            string Query = "TRUNCATE TABLE Enfant";
-
-
-            try
+            using (cnn = new SqlConnection(connetionString))
             {
-                cnn.Open();
-                SqlCommand clear = new SqlCommand(Query, cnn);
-                if (clear.ExecuteNonQuery() == 0)
-                    throw new ApplicationException("y a r de modif !");
-                else MessageBox.Show("Suppression confirmed");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
-            }
-            finally
-            {
+                string Query = "TRUNCATE TABLE Enfant";
 
-                cnn.Close();
-            }
 
+                try
+                {
+                    cnn.Open();
+                    SqlCommand clear = new SqlCommand(Query, cnn);
+                    if (clear.ExecuteNonQuery() == 0)
+                        throw new ApplicationException("y a r de modif !");
+                    else MessageBox.Show("Suppression confirmed");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
+                }
+                finally
+                {
+
+                    cnn.Close();
+                }
+
+            }
         }
         public void suppChild(int Id)
         {
-            cnn = new SqlConnection(connetionString);
-            string Query = "DELETE from Enfant where Id ="+Id;
-
-
-            try
+            using (cnn = new SqlConnection(connetionString))
             {
-                cnn.Open();
-                SqlCommand clear = new SqlCommand(Query, cnn);
-                if (clear.ExecuteNonQuery() == 0)
-                    throw new ApplicationException("y a r de modif !");
-                else MessageBox.Show("Suppression de l'Id : "+Id+" confirmed");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
-            }
-            finally
-            {
+                string Query = "DELETE from Enfant where Id =" + Id;
 
-                cnn.Close();
-            }
 
+                try
+                {
+                    cnn.Open();
+                    SqlCommand clear = new SqlCommand(Query, cnn);
+                    if (clear.ExecuteNonQuery() == 0)
+                        throw new ApplicationException("y a r de modif !");
+                    else MessageBox.Show("Suppression de l'Id : " + Id + " confirmed");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ca marche pas connard car :  " + ex.Message);
+                }
+                finally
+                {
+
+                    cnn.Close();
+                }
+
+            }
         }
       
         //public void ExportDataSetToExcel()
